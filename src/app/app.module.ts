509e16575c44923b109a7105d406fe8ece620051
Guilherme Kilian <guilherme.kilian@umbler.com>
2022-11-06 17:24:20 -0300

    'firebase-install'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,31 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArAOYKJNyw-BiAD_cxd-yhVOkgsAvMFLQ",
+  authDomain: "partyplace-19ede.firebaseapp.com",
+  databaseURL: "https://partyplace-19ede-default-rtdb.firebaseio.com",
+  projectId: "partyplace-19ede",
+  storageBucket: "partyplace-19ede.appspot.com",
+  messagingSenderId: "227456356371",
+  appId: "1:227456356371:web:f2e06b4e1d3dd65abeefe6",
+  measurementId: "G-DESF6PVDE8"
+};
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),  
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
