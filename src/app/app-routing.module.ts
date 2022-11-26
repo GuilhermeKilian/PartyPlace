e6a7676fb17c4fc6e762e4f31a0a299acc9c8a00
Guilherme Kilian <guilherme.kilian@umbler.com>
2022-11-26 19:40:51 -0300

    force login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ForceloginService } from './auth/services/forcelogin.service';
 import { LoginPage } from './login/login.page';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    canActivate: [ForceloginService]
   },
   {
     path: 'login',
