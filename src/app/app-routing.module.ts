fa848a8741ae92b9b829ed56c14f59c6d14a6a66
Guilherme Kilian <guilherme.kilian@umbler.com>
2022-11-26 16:13:17 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './login/login.page';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+     component: LoginPage
   }
 ];
 @NgModule({
